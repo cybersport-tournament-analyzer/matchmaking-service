f41eb9ab08004a5702b5762740ec1b29a80196d0
yofujitsu <yofujitsuuu@gmail.com>
2025-02-15 17:58:17 +0300

    refresh jedis pool 2

--- matchmaking-service/src/main/java/com/vkr/matchmaking_service/config/redis/RedisConfig.java
+++ matchmaking-service/src/main/java/com/vkr/matchmaking_service/config/redis/RedisConfig.java
@@ -1,10 +1,12 @@
 package com.vkr.matchmaking_service.config.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -15,9 +17,18 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    /**
+     * Настройка подключения к Redis
+     */
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
